e1b0ae77a86fcffec9c3368a38d5475b447d6a3b
Mikhail Rudenko <rudenko.michael@gmail.com>
2020-03-04 22:13:57 +0300

    Add more analytics data

--- lib/main.dart
+++ lib/main.dart
@@ -19,20 +19,30 @@ Future<OrdersStatistics> fetchOrdersStats() async {
 
 class OrdersStatistics {
   final int overallOrdersCountToday;
-  final double overallRevenueLastMonths;
+  final double revenueThisMonth;
   final double overallRevenue;
+  final int ordersCountThisMonth;
+  final int overallOrdersCount;
+  final int ordersCountPreviousMonth;
 
-  OrdersStatistics({
-    this.overallOrdersCountToday,
-    this.overallRevenueLastMonths,
-    this.overallRevenue
-  });
+  OrdersStatistics(
+      {this.overallOrdersCountToday,
+      this.revenueThisMonth,
+      this.overallRevenue,
+      this.ordersCountThisMonth,
+      this.overallOrdersCount,
+      this.ordersCountPreviousMonth});
 
   factory OrdersStatistics.fromJson(Map<String, dynamic> json) {
     return OrdersStatistics(
       overallOrdersCountToday: json['stats'][0]['overall_orders_count_today'],
-      overallRevenueLastMonths: json['stats'][0]['overall_revenue_last_months'][0],
+      revenueThisMonth: json['stats'][0]['overall_revenue_last_months'][0],
       overallRevenue: json['stats'][0]['overall_revenue'],
+      ordersCountThisMonth: json['stats'][0]['overall_orders_count_last_months']
+          [0],
+      overallOrdersCount: json['stats'][0]['overall_orders_count'],
+      ordersCountPreviousMonth: json['stats'][0]
+          ['overall_orders_count_last_months'][1],
     );
   }
 }
@@ -74,9 +84,19 @@ class _MyAppState extends State<MyApp> {
                 return Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
-                    Text('Orders today: ${snapshot.data.overallOrdersCountToday}'),
-                    Text('Revenue this month: ${snapshot.data.overallRevenueLastMonths} €'),
+                    Text(
+                        'Orders today: ${snapshot.data.overallOrdersCountToday}'),
+                    Text(
+                        'Revenue this month: ${snapshot.data.revenueThisMonth} €'),
                     Text('Overal revenue: ${snapshot.data.overallRevenue} €'),
+                    Text(
+                        'Orders count this month: ${snapshot.data.ordersCountThisMonth}'),
+                    Text(
+                        'Overal orders count: ${snapshot.data.overallOrdersCount}'),
+                    Text(
+                        'Orders count previous month: ${snapshot.data.ordersCountPreviousMonth}'),
+                    Text(
+                        'Orders count percentage compare with previous month: ${snapshot.data.ordersCountThisMonth / snapshot.data.ordersCountPreviousMonth * 100 - 100}'),
                   ],
                 );
               } else if (snapshot.hasError) {
